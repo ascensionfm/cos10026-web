eebf0e110715c57a490fb51c2a7b425be7c7f86c
Nguyen Quy Vuong <admin@ascensionfm.works>
2025-03-06 14:42:55 +0700

    checker

--- settings.php
+++ settings.php
@@ -11,4 +11,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$sql = "SELECT email, password FROM login";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Email: " . $row["email"]. " - Password: " . $row["password"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
