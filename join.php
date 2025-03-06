ee5ea1ddb891142eea836373b857a29b82413781
Nguyen Quy Vuong <admin@ascensionfm.works>
2025-03-06 14:17:46 +0700

    fixing joins

--- join.php
+++ join.php
@@ -155,6 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
+            echo "Connection failed: " . $conn->connect_error;
         }
         
         $stmt = $conn->prepare("SELECT * FROM login WHERE email = ? AND password = ?");
