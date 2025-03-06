5fcaff33a2dae681d5b8b57a92395055b1905ddd
NKH <dngminh7105@gmail.com>
2025-03-06 13:24:40 +0700

    setting dtbdtb

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+	// filename: settings.php
+	// author: Nguyen Khanh Huyen
+	// created: 06/03/25
+	// description: Imported script containing credentials to connect to the database server.
+?>
+<?php
+	// Please change this information if you are testing this website on localhost.
+	$host = "local";
+	$user = "root";
+	$pwd = "";
+	$sql_db = "104988508_db";
+?>
