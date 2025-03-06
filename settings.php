effcaf61d7829bf5b6d877708f76038e12ce4730
NKH <dngminh7105@gmail.com>
2025-03-06 15:57:05 +0700

    setting

--- settings.php
+++ settings.php
@@ -4,6 +4,7 @@
 	// created: 06/03/25
 	// description: Imported script containing credentials to connect to the database server.
 ?>
+
 <?php
 	// Please change this information if you are testing this website on localhost.
 	$host = "local";
