68e2f1021eb37517be3a8b9bcb691b1e673f3c01
Nguyen Quy Vuong <admin@ascensionfm.works>
2025-03-06 14:21:18 +0700

    settings

--- settings.php
+++ settings.php
@@ -3,4 +3,13 @@ $servername = "localhost";
 $username = "ascensionfm_";
 $password = "AscensionFM123";
 $dbname = "ascensionfm_";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
