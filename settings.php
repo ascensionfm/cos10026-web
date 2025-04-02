5b5fc2efb23ec6f5cdcf1c6e54a3d52fb8d7bc6b
Nguyen Quy Vuong <admin@ascensionfm.works>
2025-04-03 00:25:55 +0700

    main settings

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pwd = "ikon282006"; 
-$sql_db = "test";
+    $host="feenix-mariadb.swin.edu.au";
+    $user="s105551228";
+    $pwd="050806";
+    $sql_db="s105551228_db";
 ?>
